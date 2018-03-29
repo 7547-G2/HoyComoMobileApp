c936d2918539272bd6b46853b36577336ddff60c
Carlos Furnari <carlosfurnari@gmail.com>
2018-03-29 19:07:40 -0300

    adding facebook login button

--- app/src/main/java/com/grupo2/hoycomo/MainActivity.java
+++ app/src/main/java/com/grupo2/hoycomo/MainActivity.java
@@ -1,13 +1,100 @@
 package com.grupo2.hoycomo;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if (!isOnline()){
+            Context context = getApplicationContext();
+            CharSequence text = "No se detectó conexión a internet, la aplicación no podrá utilizarse";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.show();
+        } else {
+            boolean loggedIn = AccessToken.getCurrentAccessToken() != null;
+            if (!loggedIn) {
+               setContentView(R.layout.activity_main);
+            }
+            callbackManager = CallbackManager.Factory.create();
+
+            LoginManager.getInstance().registerCallback(callbackManager,
+                    new FacebookCallback<LoginResult>() {
+                        @Override
+                        public void onSuccess(LoginResult loginResult) {
+                            if (loginCorrect()){
+                                intent = new Intent(getApplicationContext(), ShopListActivity.class);
+                                startActivity(intent);
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancel() {
+                            showFacebookError();
+                        }
+
+                        @Override
+                        public void onError(FacebookException exception) {
+                            showFacebookError();
+                        }
+                    });
+            if (loggedIn){
+                Profile profile = Profile.getCurrentProfile();
+                System.out.println("nombre es : " + profile.getFirstName() + ", " + profile.getLastName());
+                System.out.println("id es : " + profile.getId());
+                System.out.println("usuario es : " + profile.getName());
+            }
+        }
+    }
+
+    private boolean loginCorrect() {
+
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    private void showFacebookError() {
+        Context context = getApplicationContext();
+        CharSequence text = "Se produjo un error al intentar conectarse con Facebook, pruebe más tarde";
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.show();
     }
 }

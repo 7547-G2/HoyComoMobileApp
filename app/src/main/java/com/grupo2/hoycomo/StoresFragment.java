29973d33fb5bea38290a405ea63733b03e64c987
Carlos Furnari <carlosfurnari@gmail.com>
2018-04-16 00:13:43 -0300

    getting stores

--- app/src/main/java/com/grupo2/hoycomo/StoresFragment.java
+++ app/src/main/java/com/grupo2/hoycomo/StoresFragment.java
@@ -10,16 +10,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 /**
  * Created by Samsung on 22/03/2018.
  */
 
 public class StoresFragment extends Fragment {
+
+    String BASE_URI = "https://hoy-como-backend.herokuapp.com/api/mobileUser";
 
     String[] shopNames;
     String[] shopPics;
@@ -30,12 +41,15 @@ public class StoresFragment extends Fragment {
 
     List<ShopItem> rowItems;
     ListView mylistview;
+    View v;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.stores, container, false);
-
+        v = inflater.inflate(R.layout.stores, container, false);
+        shopPics = getResources().getStringArray(R.array.profile_pics);
+        favoritesMock = getResources().getStringArray(R.array.favorite);
+        /*
         rowItems = new ArrayList<ShopItem>();
 
         getFavorites();
@@ -59,6 +73,9 @@ public class StoresFragment extends Fragment {
         mylistview = (ListView) v.findViewById(R.id.storesList);
         CustomAdapter adapter = new CustomAdapter(getContext(), rowItems);
         mylistview.setAdapter(adapter);
+        */
+        //getFavorites();
+        getStores();
         return v;
     }
 
@@ -97,17 +114,16 @@ public class StoresFragment extends Fragment {
     }
 
     private void getStores() {
-        /*
-        String REQUEST_TAG = "stores";
-        String url = "";
+        String REQUEST_TAG = "getstores";
+        String url = BASE_URI + "/comercios";
         // Initialize a new JsonObjectRequest instance
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
                 Request.Method.GET,
                 url,
                 null,
-                new Response.Listener<JSONObject>() {
+                new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(JSONObject response) {
+                    public void onResponse(JSONArray response) {
                         parseStores(response);
                     }
                 },
@@ -115,18 +131,38 @@ public class StoresFragment extends Fragment {
                     @Override
                     public void onErrorResponse(VolleyError error){
                         System.out.println("error 2: " + error.toString());
-                        showToastError("Error en la aplicación");
-                        finish();
+                        ErrorManager.showToastError("Error al obtener los comercios");
                     }
                 }
         );
 
         // Adding JsonObject request to request queue
-        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjectRequest,REQUEST_TAG);
-    */
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonArrayRequest,REQUEST_TAG);
     }
 
-    private void parseStores(JSONObject response) {
+    private void parseStores(JSONArray response) {
+        JSONObject comercio;
+        String name, desc1, desc2;
+        rowItems = new ArrayList<ShopItem>();
+        for (int i = 0; i < response.length(); i++) {
+            try {
+                comercio = response.getJSONObject(i);
+                System.out.println(comercio.toString());
+                name = comercio.getString("nombre");
+                name = name.substring(0, Math.min(25, name.length()));
+                desc1 = comercio.getString("tipo") + " - ";
+                desc2 = comercio.getString("leadTime") + " - Entre $" + comercio.getString("minPrice") +
+                        " y $" + comercio.getString("maxPrice");
+                ShopItem item = new ShopItem(name, shopPics[0], desc1, desc2, Boolean.valueOf(favoritesMock[0]), 3);
+                rowItems.add(item);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        mylistview = (ListView) v.findViewById(R.id.storesList);
+        CustomAdapter adapter = new CustomAdapter(getContext(), rowItems);
+        mylistview.setAdapter(adapter);
 
     }
 }

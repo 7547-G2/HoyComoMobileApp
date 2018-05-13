c95fba599ecf5993da82058d9b18e5d262d8b4c0
Carlos Furnari <carlosfurnari@gmail.com>
2018-05-13 03:57:40 -0300

    adding shopButton to menu

--- app/src/main/java/com/grupo2/hoycomo/DishActivity.java
+++ app/src/main/java/com/grupo2/hoycomo/DishActivity.java
@@ -1,13 +1,27 @@
 package com.grupo2.hoycomo;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+
+import static com.grupo2.hoycomo.ErrorManager.showToastError;
+
 public class DishActivity extends AppCompatActivity {
+
+    String BASE_URI = "https://hoy-como-backend.herokuapp.com/api/mobileUser/menu/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +41,63 @@ public class DishActivity extends AppCompatActivity {
             }
         });
 
+        Intent intent = getIntent();
+        Integer id = intent.getIntExtra("dish_id", 0);
+        //getDish(id);
+
+        final TextView number = findViewById(R.id.tvCant);
+        number.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int after, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                try {
+                    TextView subT = findViewById(R.id.tvDsubTotal);
+                    //TODO multiplicar por el valor del plato
+                    subT.setText("$ " + number.getText());
+                } catch (Exception e) {}
+            }
+        });
+
+    }
+
+    private void getDish(Integer id) {
+        String REQUEST_TAG = "getDish";
+        String url = BASE_URI + id;
+        // Initialize a new JsonObjectRequest instance
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        parseDish(response);
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        System.out.println("error getMenu: " + error.toString());
+                        showToastError("Error al obtener el menu");
+                    }
+                }
+        );
+
+        // Adding JsonObject request to request queue
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonArrayRequest,REQUEST_TAG);
+    }
+
+    private void parseDish(JSONArray response) {
     }
 
     public void increaseNum(View view) {
@@ -34,6 +105,7 @@ public class DishActivity extends AppCompatActivity {
         String aux = tvNum.getText().toString();
         Integer num = Integer.parseInt(aux);
         num++;
+        if (num > 99) {num = 99;}
         tvNum.setText(num.toString());
     }
 

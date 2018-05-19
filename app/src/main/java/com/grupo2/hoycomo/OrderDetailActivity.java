3bb8056f884acc4783a332674f13a5a034d1cac5
Carlos Furnari <carlosfurnari@gmail.com>
2018-05-19 12:46:27 -0300

    adding order detail activity

--- app/src/main/java/com/grupo2/hoycomo/OrderDetailActivity.java
+++ app/src/main/java/com/grupo2/hoycomo/OrderDetailActivity.java
@@ -1,13 +1,38 @@
 package com.grupo2.hoycomo;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.facebook.Profile;
 
 public class OrderDetailActivity extends AppCompatActivity {
+
+    String BASE_URI = "https://hoy-como-backend.herokuapp.com/api/mobileUser";
+    Integer oId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayShowCustomEnabled(true);
+        actionbar.setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        Intent intent = getIntent();
+        oId = intent.getIntExtra("order_id", 99);
     }
 }

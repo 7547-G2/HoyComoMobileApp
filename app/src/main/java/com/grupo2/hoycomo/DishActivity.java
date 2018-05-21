3c3e73c42c17aaa7f9b4e907bc67d2fa05e10301
Carlos Furnari <carlosfurnari@gmail.com>
2018-05-21 00:42:38 -0300

    adding detail of dish from backend

--- app/src/main/java/com/grupo2/hoycomo/DishActivity.java
+++ app/src/main/java/com/grupo2/hoycomo/DishActivity.java
@@ -1,29 +1,36 @@
 package com.grupo2.hoycomo;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import static com.grupo2.hoycomo.ErrorManager.showToastError;
 
 public class DishActivity extends AppCompatActivity {
 
-    String BASE_URI = "https://hoy-como-backend.herokuapp.com/api/mobileUser/menu/";
+    String BASE_URI = "https://hoy-como-backend.herokuapp.com/api/mobileUser/";
     Integer id = 0;
     Integer sId = 0;
     Integer sum = 0;
@@ -52,7 +59,7 @@ public class DishActivity extends AppCompatActivity {
         sId = intent.getIntExtra("store_id", 0);
         EditText etObs = findViewById(R.id.etObs);
         etObs.setText("");
-        //getDish(id);
+        getDish(id);
 
         final TextView number = findViewById(R.id.tvCant);
         number.addTextChangedListener(new TextWatcher() {
@@ -81,32 +88,53 @@ public class DishActivity extends AppCompatActivity {
 
     private void getDish(Integer id) {
         String REQUEST_TAG = "getDish";
-        String url = BASE_URI + id;
+        String url = BASE_URI + "/plato/" + id;
         // Initialize a new JsonObjectRequest instance
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                 Request.Method.GET,
                 url,
                 null,
-                new Response.Listener<JSONArray>() {
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(JSONArray response) {
+                    public void onResponse(JSONObject response) {
+                        System.out.println(response.toString());
                         parseDish(response);
                     }
                 },
                 new Response.ErrorListener(){
                     @Override
                     public void onErrorResponse(VolleyError error){
-                        System.out.println("error getMenu: " + error.toString());
+                        System.out.println("error getDish: " + error.toString());
                         showToastError("Error al obtener el menu");
                     }
                 }
         );
 
         // Adding JsonObject request to request queue
-        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonArrayRequest,REQUEST_TAG);
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjectRequest,REQUEST_TAG);
     }
 
-    private void parseDish(JSONArray response) {
+    private void parseDish(JSONObject response) {
+        String name = "Test", image = "default";
+        Integer id = 0;
+        try {
+            name = response.getString("nombre");
+            image = response.getString("imagen");
+            //id = response.getInt("id_plato");
+            price = response.getInt("precio");
+            image = image.split(";base64,")[1];
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        TextView tvName = findViewById(R.id.tvDname);
+        tvName.setText(name);
+        byte[] decodedString = Base64.decode(image, Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        ImageView ivImage = findViewById(R.id.ivDfoto);
+        ivImage.setImageBitmap(decodedByte);
+        TextView tvPrice = findViewById(R.id.tvDprice);
+        tvPrice.setText("$ " + String.valueOf(price));
+
     }
 
     public void increaseNum(View view) {
